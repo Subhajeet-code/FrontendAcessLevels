488d6b5de866b99739186869456e796471aab0d6
Subhajeet-code <subhajeet.maastrix@gmail.com>
2025-10-27 15:35:18 +0530

    updated for plesk

--- src/auth/Signup.jsx
+++ src/auth/Signup.jsx
@@ -71,7 +71,7 @@ const SignUpForm = () => {
       const response = await fetch(apiUrl() + "user/userRegister", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password, user_type_id: role }),
+        body: JSON.stringify({ email, password, user_type_id: role , created_by: "Self" }),
       });
 
       const data = await response.json();
